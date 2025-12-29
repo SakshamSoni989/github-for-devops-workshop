c9e094f89a0dcc41e2b97d511341a06ca725cba7
Saksham Soni <sakshamsoni6768@gmail.com>
2025-12-30 02:06:44 +0530

    Update wd.java

--- wd.java
+++ wd.java
@@ -1,5 +1,7 @@
 public class wd {
 public static void main(String[] args) {
     System.out.println("saksham soni");
+    //pull = means changes github to show in vs code
+    System.out.println("saksham soni");
 }    
 }
